789e526c9954529ceff28aa89954f6f2f4a0e714
Zakaria Davis <zakariadavis@gmail.com>
2017-09-11 12:27:38 +0300

    Reset Password header edit

--- resources/views/artist/passwords/email.blade.php
+++ resources/views/artist/passwords/email.blade.php
@@ -6,7 +6,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-success">
-                <div class="panel-heading">Reset Password</div>
+                 <div class="col-md-6 col-md-offset-4">
+                        <h3>Reset Password</h3>
+                    </div>
                 <div class="panel-body">
                     @if (session('status'))
                     <div class="alert alert-success">
