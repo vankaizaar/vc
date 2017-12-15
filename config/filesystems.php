2cee30f241274d8a518216bf8b64e9f0ce45bc13
Zakaria Davis <zakariadavis@gmail.com>
2017-12-15 09:51:54 +0300

    more updated tcg configs

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,11 +42,10 @@ return [
             'root' => storage_path('app'),
         ],
         'public' => [
-            'driver' => 'local',
-//            'root' => storage_path('app'),
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
-            'visibility' => 'public',
+//            'driver' => 'local',
+//            'root' => storage_path('app/public'),
+//            'url' => env('APP_URL') . '/storage',
+//            'visibility' => 'public',
         ],
         's3' => [
             'driver' => 's3',
